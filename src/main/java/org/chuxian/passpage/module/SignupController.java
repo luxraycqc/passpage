7381f1f9cc7a4cee4ea5cbf260dd2ae972734d29
chuxian <chulingxian@gmail.com>
2020-02-21 10:46:01 +0800

    debug

--- src/main/java/org/chuxian/passpage/module/SignupController.java
+++ src/main/java/org/chuxian/passpage/module/SignupController.java
@@ -1,7 +1,5 @@
 package org.chuxian.passpage.module;
 
-import cn.hutool.log.Log;
-import cn.hutool.log.LogFactory;
 import org.chuxian.passpage.message.SignupRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +10,9 @@ public class SignupController {
     @Autowired
     SignupService signupService;
 
-    Log log = LogFactory.get();
-
     @RequestMapping(value="/signup", method = RequestMethod.POST)
     @ResponseBody
-    public int signup(@RequestBody SignupRequest signupRequest) {
+    public String signup(@RequestBody SignupRequest signupRequest) {
         String email = signupRequest.getEmail();
         String username = signupRequest.getUsername();
         String passwordHash = signupRequest.getPasswordHash();
@@ -24,15 +20,15 @@ public class SignupController {
         if (signupService.checkEmail(domain, email) == 0) {
             if (signupService.checkUsername(domain, username) == 0) {
                 if (signupService.signup(domain, username, email, passwordHash) == 0) {
-                    return 0;
+                    return "0";
                 } else {
-                    return 3;
+                    return "3";
                 }
             } else {
-                return 2;
+                return "2";
             }
         } else {
-            return 1;
+            return "1";
         }
     }
 }
